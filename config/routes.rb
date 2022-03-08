448fba79eb1603bd262c044c69479b5bdcd8fc57
Rich Hoyt <rhoyt10@gmail.com>
2022-03-08 13:02:58 -0500

    Add user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # GET /about
 
   get "about", to: "about#index"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
   #root
   root to: "main#index"
 end
