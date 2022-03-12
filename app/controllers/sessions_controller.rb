30e9fdd069c894affbc8d55b8bff22f90e92bb82
Rich Hoyt <rhoyt10@gmail.com>
2022-03-12 11:10:46 -0500

    Add sign in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:email])
+
+		if user.present? && user.authenticate(params[:password])
+			cookies[:user_id] = user.id
+			redirect_to root_path, notice: "Logged in Successfully"
+		else
+			flash[:alert] = 'Invalid email or password'
+			render :new
+		end
+	end
+
 	def destroy
 		cookies[:user] = nil
 		redirect_to root_path, notice: "Logged out"
