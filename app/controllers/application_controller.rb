6ca577684ac7e83f7dca075f99c5e63d10746705
Rich Hoyt <rhoyt10@gmail.com>
2022-03-12 11:34:18 -0500

    Add current user attribute

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+	before_action :set_current_user
+
+	def set_current_user
+		if cookies[:user]
+			Current.user = User.find_by(id: cookies[:user])
+		end
+	end
 end
