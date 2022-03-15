ccc8124d49a40bda44cda93bee97cd26deb1b6ca
Rich Hoyt <rhoyt10@gmail.com>
2022-03-15 10:09:09 -0400

    Add password reset route and view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get "password", to: "passwords#edit", as: :edit_password
   patch "password", to: "passwords#update"
 
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
@@ -17,4 +20,5 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
   #root
   root to: "main#index"
+
 end
