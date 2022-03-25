6d30a649e7174684d4f06e3d11b2338de3778a7a
Rich Hoyt <rhoyt10@gmail.com>
2022-03-25 11:54:45 -0400

    Enable posting tweets to twitter

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -3,4 +3,13 @@ class TwitterAccount < ApplicationRecord
   has_many :tweets
 
   validates :username, uniqueness: true
+
+  def client
+		client = Twitter::REST::Client.new do |config|
+		  config.consumer_key        = Rails.application.credentials.dig(:twitter, :access_key_id)
+		  config.consumer_secret     = Rails.application.credentials.dig(:twitter, :secret_access_key)
+		  config.access_token        = token
+		  config.access_token_secret = secret
+		end
+  end
 end
